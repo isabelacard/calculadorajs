9ad4865f4a86fc019ca1b819ee66245ea7513bc7
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-03-25 03:23:39 -0500

    se crean los mensajes de error por operador y division por cero

--- calculadora.js
+++ calculadora.js
@@ -3,7 +3,29 @@ const submit = document.getElementById('submit');
 submit.addEventListener('click', function(event){
     event.preventDefault();
 
-    const op1 = parseFloat(document.getElementById('op1').value)
+    const op1 = parseInt(document.getElementById('op1').value)
     const operador = document.getElementById('operador').value
-    const op2 = parseFloat(document.getElementById('op2').value)
+    const op2 = parseInt(document.getElementById('op2').value)
+
+    let resultado;
+
+if (operador == "+") {
+    resultado = op1+op2;
+} else if (operador == "-") {
+    resultado = op1-op2;
+} else if (operador == "*") {
+    resultado = op1*op2;
+} else if (operador == "/") {
+    if (op2 == 0) {
+        resultado = "ERROR: ¡No se puede dividir por cero!";
+    } else {
+        resultado = op1/op2
+    }
+}else {
+    resultado =  "ERROR: Operador No Válido!"
+}
+
+
+document.getElementById('resultado').innerText = resultado;
+
 })
