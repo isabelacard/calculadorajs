5cc058f0d77b03040ad791903545d2e391dae5bf
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-03-25 03:18:14 -0500

    se agregan las constantes con el parseInt

--- calculadora.js
+++ calculadora.js
@@ -3,4 +3,7 @@ const submit = document.getElementById('submit');
 submit.addEventListener('click', function(event){
     event.preventDefault();
 
+    const op1 = parseFloat(document.getElementById('op1').value)
+    const operador = document.getElementById('operador').value
+    const op2 = parseFloat(document.getElementById('op2').value)
 })
